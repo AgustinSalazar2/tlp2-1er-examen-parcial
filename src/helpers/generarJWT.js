13d899000a36faab1ff7c4988a0bb7ea9f457d82
AgustinSalazar2 <agustinsssalazar@gmail.com>
2022-10-09 23:49:43 -0300

    Creacion de funcion para generar el token y el middleware para validarlo

---
+++ src/helpers/generarJWT.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+//Funcion para generar el token de autenticacion con el id del usuario y la palabra secreta:
+const generarJWT = (uid) => {
+    return new Promise( (resolve, reject) => {
+        jwt.sign(uid, process.env.SECRET, (err, token) => {
+            if (error) {
+                reject('No se pudo generar el token');
+            } else {
+                resolve(token);
+            }
+        });
+    });
+}
+
+module.exports = generarJWT;
---
+++ src/middlewares/validarJWT.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+//Middleware para validar token
+const validarJWT = async (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {   //Se verifica si existe el token
+        return res.json({
+            msg: 'Error de autenticacion  -  Token no valido'
+        });
+    }
+
+    try {
+        const { uid } = await jwt.verify(token, process.env.SECRET)
+        const user = await User.findById(uid)
+        
+        if (!user) {     // Se busca el usuario en la base de datos para saber si pertenece al sistema
+            return res.json({
+                error: 'Token no válido - usuario no existe en BD'
+            });
+        }
+       
+        if (!user.active) {     // Verificar si el usuario está activo
+            return res.json({
+                msg: 'Token no válido - usuario con estado false'
+            });
+        }
+
+        // Se añade la información del usuario al request para que pueda ser utilizada en el resto de middlwares
+        req.user = usuario;
+
+        next();      // Se continúa con la ejecución del resto de la petición
+    } catch (error) {
+        console.log(error);
+        res.json({
+            msg: 'Error de autenticación - Token no válido'
+        })
+    }
+}
+
+module.exports = validarJWT;
